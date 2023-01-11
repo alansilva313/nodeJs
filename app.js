4f9266fd6f862ac26ab84781d07cd632ae6623a9
ALAN SILVA <96753536+alansilva313@users.noreply.github.com>
2023-01-11 16:44:44 -0300

    upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res) => {
+
+
+
+    res.render('home')
+
+})
+
+app.listen(3000, (err) => {
+    if(err){
+        console.log(err)
+    }
+    console.log('Conectado com Sucesso!')
+})
+
+
+
